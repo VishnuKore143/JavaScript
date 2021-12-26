7b90e3168a78d1df0686a498260fad3f7cb67adc
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-26 23:04:36 +0530

    Update 01ConsoleLogs.js

--- 01ConsoleLogs.js
+++ 01ConsoleLogs.js
@@ -1,11 +1,11 @@
 console.time("mycode took");
 console.log("Hello user");
-console.log(4+35);
-console.log(true);
-console.log([2,8,7,9]);
-console.log({vishnu: 'sub',marks:89});
-console.table({vishnu: 'student', marks:78});
-console.warn("This is a warning");
+console.log(4+35); // integers 
+console.log(true); // boolean in js
+console.log([2,8,7,9]); //array in js
+console.log({vishnu: 'sub',marks:89}); // method in js
+console.table({vishnu: 'student', marks:78}); // table in js
+console.warn("This is a warning"); // warning in js
 //console.clear();
 console.timeEnd("Your code took");
 // console.assert(566<189, 'Age >189 This is not possible');
