f7fabfdd51911f9b205ec582f77ec5dbeffcf4d8
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-26 16:55:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.time("mycode took");
+console.log("Hello user");
+console.log(4+35);
+console.log(true);
+console.log([2,8,7,9]);
+console.log({vishnu: 'sub',marks:89});
+console.table({vishnu: 'student', marks:78});
+console.warn("This is a warning");
+//console.clear();
+console.timeEnd("Your code took");
+// console.assert(566<189, 'Age >189 This is not possible');
+console.error('This is an error')
+
