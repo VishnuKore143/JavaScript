368d1ba676bbd2e0d0ac104e69736c2a9b64f9f5
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-27 02:26:17 +0530

    Add files via upload

---
+++ 03DataType.js
@@ -0,0 +1,50 @@
+// Premitive Data Types
+
+// String
+let Name = "vishnu";
+console.log("my string is : "+Name)
+console.log("Data Type of string is : "+(typeof Name));
+
+// Numbers 
+let marks = 34;
+console.log("Data type marks is : "+ typeof marks);
+
+// Boolean
+let isDriver = true;
+console.log("Data type Boolean is : "+ typeof isDriver);
+
+// Null
+let nullVar = null;
+console.log("Data type null is : "+ typeof nullVar);
+
+// Undefine
+let undef;
+console.log("Data type of undefine is : "+ typeof undef);
+
+
+
+// Refrene Data Type 
+
+//Arrays
+myarr = [1,5,4,8,false,"vishnu"];
+console.log("Data type of array is : "+ typeof myarr);
+
+// object literils
+
+let stMarks = {
+    vishnu: 89,
+    abhi: 36,
+    ajay: 45
+}
+console.log("Data type of object literils is : "+ typeof stMarks);
+
+// Function 
+
+function findname(){
+    
+}
+console.log("Data type of function is : "+ typeof findname);
+
+// Date
+let date = new Date();
+console.log("Data type of date  is : "+ typeof date);
