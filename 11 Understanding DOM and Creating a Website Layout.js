a2689d3d2c5eaf74302ab74b9f10fc4f20842bb1
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-01-26 02:50:19 +0530

    Add files via upload

---
+++ 11 Understanding DOM and Creating a Website Layout.js
@@ -0,0 +1,33 @@
+console.log('Understing DOM and Creating a website Layout.');
+
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0]; //HTML collection
+//  Array.from(a).forEach(function(element){
+//     console.log(element);
+// });
+// a=document.links[0].href;
+
+//Quick Quiz
+// use document.images and document.script and print the list of images and script on html pages.
+
+
+
+
+Array.from(a).forEach(function (element) {
+    console.log(document.scripts)
+})
+
+// m = document.images;
+// s = document.scripts;
+
+// Array.from(m).forEach(function(element){
+//     console.log(element);
+// });
+
+// Array.from(s).forEach(function(element){
+//     console.log(element);
+// });
+
+console.log(a);
