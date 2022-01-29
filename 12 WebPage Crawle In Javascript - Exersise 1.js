6f50c8e226d55276585b8f9ad0138900bff1939e
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-01-30 02:40:54 +0530

    Update 12 WebPage Crawle In Javascript - Exersise 1.js

--- 12 WebPage Crawle In Javascript - Exersise 1.js
+++ 12 WebPage Crawle In Javascript - Exersise 1.js
@@ -1,12 +1,17 @@
 console.log('WebPage Crawle In Javascript - Exersise 1');
 
+// Question : you have to creat a variable which is a string containing gthe text which is a link you are interested in.
+// You have to fetch all the links from a given page which containing this text 'facebook'. else 'link not found'.
 
 let z = document.links;
-let fb = 'facebook'
-// let googl = 'google'
+let fb = 'facebook';
+// let googl = 'google';
 Array.from(z).forEach(function(element){
         if(String(element).includes(fb)){
             console.log(element);
         } 
+        else{
+                console.log('Link Not Found');
+        }
         
     })
